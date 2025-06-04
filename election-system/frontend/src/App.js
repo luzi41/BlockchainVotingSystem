b51795d9078e2219c7d9418bcbff4ec23248cca8
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-04 22:55:49 +0200

    Add files via upload

--- election-system/frontend/src/App.js
+++ election-system/frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Start from "./components/Start"
 import VoteForm from "./components/VoteForm";
 import Results from "./components/Results";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
       <nav>
         <ul>
-          <li><Link to="/">Abstimmen</Link></li>
+          <li><Link to="/">Start</Link></li>
+          <li><Link to="/vote">Abstimmen</Link></li>
           <li><Link to="/results">Ergebnisse</Link></li>
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<VoteForm />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/vote" element={<VoteForm />} />
         <Route path="/results" element={<Results />} />
       </Routes>
     </Router>
