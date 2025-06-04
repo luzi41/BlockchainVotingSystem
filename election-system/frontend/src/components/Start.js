23f76319fe76a7bea3b8300969a61a8df2b586f7
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-04 22:51:16 +0200

    Add files via upload

---
+++ election-system/frontend/src/components/Start.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+function Start() {
+  return (
+    <div>
+      <h2>Wahl 2xxx</h2>
+      <h3>Registrieren für die Online-Wahl</h3>
+      <h3>Was wird gewählt</h3>
+      <h3>Wer steht zur Wahl - KandidatInnen in Ihrem Wahlbezirk</h3>
+      <h3>Die Partien und ihre Wahlprogramme</h3>
+      <h3>Das neue Wahlgesetz</h3>
+    </div>
+  );
+}
+
+export default Start;
