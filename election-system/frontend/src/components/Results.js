8538753e6bfdf12c7e3b140be974be7bdfb9d238
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-05 20:33:43 +0200

    Update Results.js

--- election-system/frontend/src/components/Results.js
+++ election-system/frontend/src/components/Results.js
@@ -3,24 +3,29 @@ import { BrowserProvider, Contract } from "ethers";
 import Election from "../artifacts/contracts/Election.sol/Election.json";
 
 const ethers = require("ethers");
-const contractAddress = "0x00f..."; // Adresse des deployten Contracts
+
+const contractAddress = "0x00..."; // Adresse des deployten Contracts
 
 function Results() {
   const [winner, setWinner] = useState("");
   const [votes, setVotes] = useState(0);
-  const [totalVotes, setTotalVotes] = useState(0); 
+  const [status, setStatus] = useState("");
+  const [totalVotes, setTotalVotes] = useState(0);
 
   useEffect(() => {
     async function fetchResults() {
       if (window.ethereum) {
         const provider = new BrowserProvider(window.ethereum);
         const contract = new ethers.Contract(contractAddress, Election.abi, provider);
+
         try {
           const [winnerName, winnerVoteCount] = await contract.getWinner();
+          //const total = await contract.getTotalVotes();
           setWinner(winnerName);
-          setVotes(winnerVoteCount.toNumber());
-          //setTotalVotes(totalVoteCount.toNumber()); // Contract muss erneut kompiliert werden.
+          setVotes(winnerVoteCount);
+          
         } catch (err) {
+          setStatus("❌ Fehler: " , err);
           console.error("Fehler beim Abrufen der Ergebnisse:", err);
         }
       }
@@ -33,7 +38,8 @@ function Results() {
       <h2>Wahlergebnisse</h2>
       <p>Sieger: {winner}</p>
       <p>Stimmen: {votes}</p>
-      <p>Total: {totalVotes}</p>
+      
+      <p>{status}</p>
     </div>
   );
 }
