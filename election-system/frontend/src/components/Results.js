66e007a40715963eb1faf46a1ad3149a2410818a
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-05 15:17:35 +0200

    Update Results.js

--- election-system/frontend/src/components/Results.js
+++ election-system/frontend/src/components/Results.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserProvider, Contract } from "ethers";
 import Election from "../artifacts/contracts/Election.sol/Election.json";
-
+import contractAddress from "../deployment-address.txt"
 const ethers = require("ethers");
-
-const contractAddress = "0x..."; // Adresse des deployten Contracts
+// const contractAddress = "0x38Fef9ccf..."; // Adresse des deployten Contracts
 
 function Results() {
   const [winner, setWinner] = useState("");
   const [votes, setVotes] = useState(0);
+  const [totalVotes, setTotalVotes] = useState(0); // Contract muss erneut kompiliert werden.
 
   useEffect(() => {
     async function fetchResults() {
@@ -19,6 +19,7 @@ function Results() {
           const [winnerName, winnerVoteCount] = await contract.getWinner();
           setWinner(winnerName);
           setVotes(winnerVoteCount.toNumber());
+          //setTotalVotes(totalVoteCount.toNumber()); // Contract muss erneut kompiliert werden.
         } catch (err) {
           console.error("Fehler beim Abrufen der Ergebnisse:", err);
         }
@@ -32,6 +33,7 @@ function Results() {
       <h2>Wahlergebnisse</h2>
       <p>Sieger: {winner}</p>
       <p>Stimmen: {votes}</p>
+      <p>Total: {totalVotes}</p>
     </div>
   );
 }
