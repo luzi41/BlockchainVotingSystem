d407a13d42c6fbf2e06352bc1857bcb6b57291e4
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-07 11:45:07 +0200

    Add files via upload

---
+++ election-system/frontend/src/config.js
@@ -0,0 +1,7 @@
+export const CONTRACT_ADDRESSES = {
+  registry: "0x0bf77110A0a4a67f36C1115d58D48E612968Cf06", // Adresse aus Deployment
+  ledger: "0x...",
+  tally: "0x...",
+};
+
+export const PROVIDER_URL = "http://localhost:8545"; // oder RPC-URL deiner Quorum-Node
