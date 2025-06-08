28415dfcc3e2b540980ae11cc0216bfda5074d35
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-08 15:15:15 +0200

    Add files via upload

--- election-system/frontend/src/App.js
+++ election-system/frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <li><Link to="/">Start</Link></li>
           <li><Link to="/vote">Abstimmen</Link></li>
           <li><Link to="/results">Ergebnisse</Link></li>
-          <li class="title"><Link to="https://github.com/luzi41/BlockchainVotingSystem/tree/v0.4" target="_blank">Blockchain Voting System 0.4</Link></li>
+          <li class="title"><Link to="https://github.com/luzi41/BlockchainVotingSystem/tree/v0.6" target="_blank">Blockchain Voting System 0.6</Link></li>
         </ul>
       </nav>
 
