00731302a8d4cc55e32ea8a11a96c8390bd89acc
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-08 14:29:06 +0200

    Add files via upload

--- election-system/contracts/Election.sol
+++ election-system/contracts/Election.sol
@@ -59,16 +59,18 @@ contract Election {
         return registeredTokens[keccak256(abi.encodePacked(_token))] && !usedTokens[keccak256(abi.encodePacked(_token))];
     }
 
-    function markTokenUsed(string memory _token) public onlyAdmin {
+    function markTokenUsed(string memory _token) public {
         require(registeredTokens[keccak256(abi.encodePacked(_token))], "Token not registered");
         require(!usedTokens[keccak256(abi.encodePacked(_token))], "Token already used");
         usedTokens[keccak256(abi.encodePacked(_token))] = true;
     }    
 
+    /*
     function registerVoter(address _voter) public onlyAdmin {
         require(!voters[_voter].registered, unicode"Wähler ist bereits registriert.");
         voters[_voter] = Voter({registered: true, hasVoted: false});
     }
+    */
 
     function startVoting() public onlyAdmin {
         require(candidates.length >= 2, "Mindestens zwei Kandidaten erforderlich.");
@@ -80,13 +82,13 @@ contract Election {
     }
 
     function vote(uint _candidateIndex, string memory _token) public onlyDuringVoting {
-        Voter storage sender = voters[msg.sender];
-        require(sender.registered, unicode"Nicht registrierter Wähler.");
-        require(!sender.hasVoted, unicode"Wähler hat bereits abgestimmt.");
+        // Voter storage sender = voters[msg.sender];
+        // require(sender.registered, unicode"Nicht registrierter Wähler.");
+        // require(!sender.hasVoted, unicode"Wähler hat bereits abgestimmt.");
         require(_candidateIndex < candidates.length, unicode"Ungültiger Kandidat.");
         require(isTokenValid(_token), "Invalid or used token");
 
-        sender.hasVoted = true;
+        // sender.hasVoted = true;
         markTokenUsed(_token);
         candidates[_candidateIndex].voteCount += 1;
     }
