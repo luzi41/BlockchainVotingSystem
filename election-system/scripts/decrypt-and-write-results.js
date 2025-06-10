f50f85c74e8139c5c0479339afddd9d598d4b6ad
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-10 19:28:32 +0200

    Add files via upload

--- election-system/scripts/decrypt-and-write-results.js
+++ election-system/scripts/decrypt-and-write-results.js
@@ -1,30 +1,32 @@
 const fs = require("fs");
 const path = require("path");
-const { ethers } = require("ethers");
+const { Wallet, JsonRpcProvider, Contract } = require("ethers");
 const forge = require("node-forge");
 
 // Config laden
-const abi = require(__dirname, "../api/Election.json").abi;
-const contractAddress = fs.readFileSync(__dirname, "../api/deployment-address.txt", "utf8").trim();
 const PRIVATE_KEY_PATH = path.join(__dirname, "../keys/private.pem");
+const contractAddress = fs.readFileSync("../api/deployment-address.txt", "utf8").trim();
+const abi = require("../api/Election.json").abi;
 const RESULT_OUTPUT_PATH = path.join(__dirname, "../frontend/src/results/aggregated.json");
+
 
 // 1. Private Key laden
 const privateKeyPem = fs.readFileSync(PRIVATE_KEY_PATH, "utf8");
 const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
 
+
 // 2. Contract verbinden
-const provider = new ethers.JsonRpcProvider("http://localhost:8545");
-const contract = new ethers.Contract(contractAddress, abi, provider);
+const provider = new JsonRpcProvider("http://localhost:8545");
+const contract = new Contract(contractAddress, abi, provider);
+
 
 // 3. Stimmen abrufen und entschlüsseln
 async function decryptVotes() {
-  //const voteCount = await contract.getVoteCount();
   const decryptedVotes = [];
   const encryptedVotes = await contract.getEncryptedVotes();
 
   for (let i = 0; i < encryptedVotes.length; i++) {
-    const encryptedVote = encryptedVotes(i);
+    const encryptedVote = encryptedVotes[i];
     const encryptedBytes = Buffer.from(encryptedVote, "base64");
 
     try {
