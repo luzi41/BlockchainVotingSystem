1ca259115ef67cf3dc3cec3bacb53211ac514ddb
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-11 20:21:38 +0200

    Update Results.js

--- election-system/frontend/src/components/Results.js
+++ election-system/frontend/src/components/Results.js
@@ -3,13 +3,19 @@ import React, { useState, useEffect } from "react";
 function Results() {
   const [status, setStatus] = useState("Ergebnisse folgen nach Entschlüsselung durch Wahlleiter.");
   const [html, setHtml] = useState("");
+  const [timestamp, setTimestamp] = useState("");
+  const [tx, setTx] = useState("");
+  const [signature, setSignature] = useState("");
 
   useEffect(() => {
       async function fetchResults() {
       try {
         const results = await import('../results/aggregated.json');
+        setTx = "";
+        setSignature = "";
+
         const htmlContent = (
-          <div>
+          <div class="border">
             <h2>Wahlergebnisse</h2>
             <ul>
               {Object.entries(results.default).map(([name, count]) => (
@@ -18,6 +24,7 @@ function Results() {
                 </li>
               ))}
             </ul>
+            <div>Zeitstempel:{timestamp} <br />Transaction: {tx}<br />Signature: {signature}</div>
           </div>
         );
         setHtml(htmlContent);
@@ -25,7 +32,7 @@ function Results() {
         
         console.error('Fehler beim Laden des Moduls:', err);
         setHtml (
-          <div>
+          <div class="border">
             <h2>Wahlergebnisse</h2>
             <p>{status}</p>
           </div>
