3f653d99e02e28ac2da44e1c6e240b9797057cd2
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2025-06-14 08:39:31 +0200

    Update deployElection.js

--- election-system/scripts/deployElection.js
+++ election-system/scripts/deployElection.js
@@ -1,25 +1,33 @@
 const fs = require("fs");
 const hre = require("hardhat");
+const prompt = require('prompt');
 
-async function main() {
-  // Admin-Wallet laden
-  const keystore = fs.readFileSync("quorum-test-network/config/nodes/member1/accountKeystore", "utf8");
-  const password = fs.readFileSync("quorum-test-network/config/nodes/member1/accountPassword", "utf8").trim();
-  const wallet = await hre.ethers.Wallet.fromEncryptedJson(keystore, password);
+prompt.start();
 
-  // Verbindung zum Quorum-Netzwerk
-  const provider = new hre.ethers.JsonRpcProvider("http://localhost:8545");
-  const admin = wallet.connect(provider);
+prompt.get(['PathToQuorum'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  PathToQuorum: ' + result.PathToQuorum);
 
-  // Contract factory und Deployment
-  const Election = await hre.ethers.getContractFactory("Election", admin);
-  const election = await Election.deploy();
-  
-  console.log(election.target);
-  
-}
+    async function main() {
+    // Admin-Wallet laden
+    const keystore = fs.readFileSync(result.PathToQuorum + "/config/nodes/member1/accountKeystore", "utf8");
+    const password = fs.readFileSync(result.PathToQuorum + "/config/nodes/member1/accountPassword", "utf8").trim();
+    const wallet = await hre.ethers.Wallet.fromEncryptedJson(keystore, password);
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    // Verbindung zum Quorum-Netzwerk
+    const provider = new hre.ethers.JsonRpcProvider("http://localhost:8545");
+    const admin = wallet.connect(provider);
+
+    // Contract factory und Deployment
+    const Election = await hre.ethers.getContractFactory("Election", admin);
+    const election = await Election.deploy();
+    
+    console.log("Contract address: " + election.target);
+    }
+
+    main().catch((error) => {
+      console.error(error);
+      process.exitCode = 1;
+    });    
 });
