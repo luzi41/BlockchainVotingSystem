f573dc82c2a820f16d6dd3b31c3aec7197ab1c78
luzi41 <louis.goettertz@gmail.com>
2025-06-15 09:13:43 +0200

    update v0.13.1

--- election-system/admin-ui/src/App.js
+++ election-system/admin-ui/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [status, setStatus] = useState("");
+  const [title, setTitle] = useState("BVS-Admin UI");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Start</Link></li>
+          <li class="title"><Link to="https://github.com/luzi41/BlockchainVotingSystem" target="_blank">Blockchain Voting System 0.13</Link></li>
+        </ul>
+      </nav>
+
+      <div id="app">
+        <h1>{title}</h1>
+        <span id="ContractAddress">{status}</span>
+      </div>
+
+      <Routes>
+        
+
+      </Routes>
+    </Router>
   );
 }
 
