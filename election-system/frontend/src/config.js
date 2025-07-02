da869a195f05dcd45e87ffb0bc1d93db12402e38
luzi41 <louis.goettertz@gmail.com>
2025-07-02 15:28:59 +0200

    v0.19.6

--- election-system/frontend/src/config.js
+++ election-system/frontend/src/config.js
@@ -1,5 +1,5 @@
 export const CONTRACT_ADDRESSES = {
-  registry: "0x21b8e25160E6a50b150495A638C9c76D72E92989", // Contract address
+  registry: "0x00fFD3548725459255f1e78A61A07f1539Db0271", // Contract address
 };
 
 export const PROVIDER_URL = "http://localhost:8545"; // oder RPC-URL deiner Quorum-Node
