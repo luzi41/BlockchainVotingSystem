5c903c59909a6e54f7537722ee1439df57d2ab7d
luzi41 <louis.goettertz@gmail.com>
2025-08-31 23:44:39 +0200

    v0.29.12

--- election-system/nextFE/bvs/src-tauri/src/main.rs
+++ election-system/nextFE/bvs/src-tauri/src/main.rs
@@ -3,7 +3,20 @@
     windows_subsystem = "windows"
 )]
 
+use std::process::{Command, Stdio};
+
 fn main() {
+    // Stelle sicher, dass Next.js im Hintergrund gestartet wird (Production)
+    #[cfg(not(debug_assertions))]
+    {
+        Command::new("npm")
+            .args(&["run", "start"])  // Für den Production-Server
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Konnte Next.js nicht starten");
+    }
+
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("Fehler beim Starten der Tauri-App");
