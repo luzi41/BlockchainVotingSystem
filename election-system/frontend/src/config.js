b6e5f439e6f9e2fc4f046e480f20d75ffb33d38d
luzi41 <louis.goettertz@gmail.com>
2025-09-02 12:14:55 +0200

    v0.29.18

--- election-system/frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-export const CONTRACT_ADDRESSES = {
-  registry: "0x00fFD3548725459255f1e78A61A07f1539Db0271", // Contract address
-};
-
-export const PROVIDER_URL = "http://localhost:8545"; // oder RPC-URL deiner Quorum-Node
-
-export const Election = "./artifacts/contracts/Bundestagswahl.sol/Bundestagswahl.json";
