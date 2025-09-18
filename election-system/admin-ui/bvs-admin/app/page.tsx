c13df2848f95172595b1b68e6cdd7bfdacf302fe
luzi41 <louis.goettertz@gmail.com>
2025-09-18 09:35:27 +0200

    v0.50.8

--- election-system/admin-ui/bvs-admin/app/page.tsx
+++ election-system/admin-ui/bvs-admin/app/page.tsx
@@ -1,19 +1,12 @@
 //import Image from "next/image";
+import Start from "@components/home";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2>Blockchain Voting Admin</h2>  
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">New election or survey</li>
-          <li className="mb-2 tracking-[-.01em]">Edit election or survey</li>
-          <li className="mb-2 tracking-[-.01em]">User/Roles Administration</li>
-          <li className="mb-2 tracking-[-.01em]">Help</li>
-        </ol>
-
+    <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+       <main className="w-full max-w-3xl">
+        <Start electionDistrict="default"  />
       </main>
-
     </div>
   );
 }
